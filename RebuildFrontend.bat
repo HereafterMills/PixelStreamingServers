a5e570546d5647512cbf7ad7f9225881492fa382
Raahil <raahil@hasdesk.com>
2024-03-14 13:56:55 +0530

    NewBuild

--- RebuildFrontend.bat
+++ RebuildFrontend.bat
@@ -4,7 +4,9 @@ set "cmdPath=%~dp0SignallingWebServer\platform_scripts\cmd"
 
 cd %uiPath%
 call npm install > "%origin%build1.log"
+call npm audit fix >> "%origin%build1.log"
 call npm run build >> "%origin%build1.log"
 
 cd %cmdPath%
+call npm audit fix >> "%origin%build1.log"
 build.bat --build >> "%origin%build1.log"
